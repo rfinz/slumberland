cc0c99e3355599ba3af5144cfb1be87acd9c4cd9
Raymond Finzel <prometheoid@gmail.com>
2025-05-16 16:29:31 -0500

    print cell contents instead of placeholder

--- src/main.rs
+++ src/main.rs
@@ -33,13 +33,25 @@ fn App() -> Element {
     //top.pop_accursed();
     //println!("-----> AFTER POP");
     memories::memorize(&top, curse.clone());
+    let contents = top.iter_rank(curse.clone()).map(|i| {
+        let ct = Box::new(i).as_content();
+        match ct {
+            CellType::Value(v) => v, 
+            CellType::Function(f) => f, 
+            CellType::Monad(m) => m, 
+            CellType::Redirect => "Redirect".to_string(), 
+            CellType::Vertex => "Vertex".to_string(), 
+            CellType::Preload => "Preload".to_string() 
+        }
+    });
     
+    tracing::debug!("Rendering!");
     rsx! {
         document::Stylesheet { href: CSS }
-        {
-            top.iter_rank(curse.clone()).map(|i| rsx! {
-                div { class: "boxy", "test" }
-            })
+        div {
+            for c in contents {
+                div { class: "cell", "{c}"}
+            }
         }
     }
            
